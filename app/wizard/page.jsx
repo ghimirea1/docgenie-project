fd7473332a2e8884b8c5008e98dd3a727bab478d
Ashish Hans Catada Ghimire <93694081+ToastedCake@users.noreply.github.com>
2023-05-17 18:08:49 -0700

    Update page.jsx

--- app/wizard/page.jsx
+++ app/wizard/page.jsx
@@ -3,17 +3,15 @@
 import "./styles.css";
 import dynamic from 'next/dynamic'
 
+const SurveyComponent = dynamic(() => import('@/components/survey/SurveyComponent'), { ssr: false });
+
 const Survey = () => {
-  const SurveyComponent = dynamic(
-    () => import('@/components/survey/SurveyComponent'),
-    { ssr: false }
-  )
   return (
       <div id="float-parent" className="grid-container">
         <div id="surveyElement" className="grid-child purple">
           <SurveyComponent />
         </div>
-        <div id="pdf-preview" class="grid-child green"></div>
+        <div id="pdf-preview" className="grid-child green"></div>
     </div>
   )
 }
