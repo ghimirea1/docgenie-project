63ca04ee0fc0d2e081e25093bc506e843666bd61
Ashish Hans Catada Ghimire <93694081+ToastedCake@users.noreply.github.com>
2023-05-21 23:12:40 -0700

    Create DeleteButton.tsx

---
+++ components/case/DeleteButton.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { TransitionStartFunction, useTransition } from "react";
+import { useRouter } from "next/navigation";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
+
+type DeleteButtonProps = { id: string };
+
+const deleteNote = async (
+  id: string,
+  router: AppRouterInstance,
+  startTransition: TransitionStartFunction,
+) => {
+  const res = await fetch(`https://next13-notes-app-api-production.up.railway.app/notes/${id}`, {
+    method: "delete",
+  });
+
+  if (res.ok) {
+    startTransition(() => {
+      router.replace("/");
+      router.refresh();
+    });
+  }
+};
+
+const DeleteButton = ({ id }: DeleteButtonProps) => {
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+
+  return (
+    <button
+      className="delete-button delete-button--solid"
+      onClick={() => deleteNote(id, router, startTransition)}
+      disabled={isPending}
+    >
+      {isPending ? "Deleting case..." : "Delete Case"}
+    </button>
+  );
+};
+
+export default DeleteButton;
