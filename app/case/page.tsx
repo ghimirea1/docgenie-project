65d643addb87dff09a10e6e1a2ff00ae5b2f7fb4
Ashish Hans Catada Ghimire <93694081+ToastedCake@users.noreply.github.com>
2023-05-21 23:12:09 -0700

    Create page.tsx

---
+++ app/case/page.tsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+    return (
+      <div className="note-viewer">
+        <div className="note--empty-state">
+          <span className="note-text--empty-state">🚀🚀🚀</span>
+        </div>
+      </div>
+    );
+  };
+  
+  export default HomePage;
