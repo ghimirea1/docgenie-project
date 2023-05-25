163d070dca36e422ddec866b2d6735ff68d64644
Ashish Hans Catada Ghimire <93694081+ToastedCake@users.noreply.github.com>
2023-05-24 22:54:23 -0700

    Update page.jsx

--- app/case/edit/[id]/page.jsx
+++ app/case/edit/[id]/page.jsx
@@ -1,6 +1,4 @@
 import prisma from "@/lib/prisma"
-import CaseEditor from "@/components/case/CaseEditor";
-
 import dynamic from 'next/dynamic'
 const SurveyComponent = dynamic(() => import('@/components/survey/CaseCreationSurveyComponent'), { ssr: false });
 
@@ -16,7 +14,6 @@ const CaseEditPage = async ({ params: { id } }) => {
     const { caseId = id, title, body, createdAt, data } = await fetchSingleCase(id);
 
     return (
-      // <CaseEditor caseId={caseId} initialBody={body} initialTitle={title} createdAt={createdAt} />
       <SurveyComponent
       id={id}
       data={data}
