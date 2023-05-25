6b5479e4c766db2a231173c4024dc04c94e94ac3
Ashish Hans Catada Ghimire <93694081+ToastedCake@users.noreply.github.com>
2023-05-24 22:54:29 -0700

    Create page.jsx

---
+++ app/case/warrant/create/page.jsx
@@ -0,0 +1,18 @@
+'use client'
+import "@/app/globals.css"
+import dynamic from 'next/dynamic'
+
+const SurveyComponent = dynamic(() => import('@/components/survey/WarrantSurveyComponent'), { ssr: false });
+
+export default async function Survey () { 
+  return (
+      <div id="float-parent" className="grid-container">
+          <div id="surveyElement" className="grid-child">
+            <SurveyComponent 
+            Templates={null} />
+          </div>
+            <div id="pdf-preview" className="grid-child">
+        </div>
+      </div>
+  )
+}
