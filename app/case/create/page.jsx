53a07ad45b2627cd3560ee74f7dd8dc4d9f68ae0
Ashish Hans Catada Ghimire <93694081+ToastedCake@users.noreply.github.com>
2023-05-24 22:51:30 -0700

    Update page.jsx

--- app/case/create/page.jsx
+++ app/case/create/page.jsx
@@ -3,17 +3,11 @@
 import "../../globals.css"
 import dynamic from 'next/dynamic'
 
-const SurveyComponent = dynamic(() => import('@/components/survey/SurveyComponent'), { ssr: false });
+const SurveyComponent = dynamic(() => import('@/components/survey/CaseCreationSurveyComponent'), { ssr: false });
 
-const Survey = () => {
+export default async function Survey () { 
   return (
-      <div id="float-parent" className="grid-container">
-          <div id="surveyElement" className="grid-child">
-            <SurveyComponent />
-          </div>
-            <div id="pdf-preview" className="grid-child">
-        </div>
-      </div>
+    <SurveyComponent 
+    data={null} />
   )
 }
-export default Survey
