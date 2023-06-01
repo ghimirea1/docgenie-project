ce43c74cdf3b22c082ba75cb8522f4c614944172
Ashish Hans Catada Ghimire <93694081+ToastedCake@users.noreply.github.com>
2023-06-01 13:06:39 -0700

    Update page.jsx

--- app/case/warrant/create/page.jsx
+++ app/case/warrant/create/page.jsx
@@ -1,16 +1,21 @@
 'use client'
 import "@/app/globals.css"
 import dynamic from 'next/dynamic'
+import { useRouter } from "next/navigation";
 
 const SurveyComponent = dynamic(() => import('@/components/survey/WarrantSurveyComponent'), { ssr: false });
 
-export default async function Survey () { 
+export default function Survey () { 
+  const router = useRouter();
+  
   return (
       <div id="float-parent" className="grid-container">
           <div id="survey-element" className="grid-child">
             <SurveyComponent 
             id={null}
-            data={null} />
+            data={null}
+            router={router}
+            />
           </div>
             <div id="pdf-preview" className="grid-child">
         </div>
