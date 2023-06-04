f6e056543ebccfffd7e336bb07e47081c262b72c
James Yao-Smith <jyaosmit@uci.edu>
2023-06-03 17:51:38 -0700

    Fixed sign-in window

--- components/layout/sign-in-modal.tsx
+++ components/layout/sign-in-modal.tsx
@@ -22,8 +22,9 @@ const SignInModal = ({
 
   return (
     <Modal showModal={showSignInModal} setShowModal={setShowSignInModal}>
-      <div className="w-full overflow-hidden shadow-xl md:max-w-md md:rounded-2xl md:border md:border-gray-200">
-        <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center md:px-16">
+      <div className="flex flex-col content-center flex items-center align-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center md:px-16">
+
+          <div>
           <a href="https://precedent.dev">
             <Image
               src="/logo.png"
@@ -33,22 +34,23 @@ const SignInModal = ({
               height={20}
             />
           </a>
-          <h3 className="font-display text-2xl font-bold">Sign In</h3>
-          <p className="text-sm text-gray-500">
+          </div>
+          <h3 className="">Sign In</h3>
+          <p className="">
             This is strictly for demo purposes - only your email and profile
             picture will be stored.
           </p>
-        </div>
+        
 
-        <div className="flex flex-col space-y-4 bg-gray-50 px-4 py-8 md:px-16">
+        <div className="">
           <button
             disabled={signInClicked}
             className={`${
               signInClicked
-                ? "cursor-not-allowed border-gray-200 bg-gray-100"
-                : "border border-gray-200 bg-white text-black hover:bg-gray-50"
-            } flex h-10 w-full items-center justify-center space-x-3 rounded-md border text-sm shadow-sm transition-all duration-75 focus:outline-none`}
-            onClick={() => {
+              ? "cursor-not-allowed border-gray-200 bg-gray-100"
+              : "border border-gray-200 bg-white text-black hover:bg-gray-50"
+          } flex h-10  items-center text-center justify-center space-x-3 rounded-md border text-sm shadow-sm transition-all duration-75 focus:outline-none`}
+          onClick={() => {
               setSignInClicked(true);
               signIn("google");
             }}
@@ -62,8 +64,8 @@ const SignInModal = ({
               </>
             )}
           </button>
+          </div>
         </div>
-      </div>
     </Modal>
   );
 };
--- components/shared/modal.tsx
+++ components/shared/modal.tsx
@@ -44,35 +44,7 @@ export default function Modal({
       {showModal && (
         <>
           {isMobile && <Leaflet setShow={setShowModal}>{children}</Leaflet>}
-          {isDesktop && (
-            <>
-              <FocusTrap focusTrapOptions={{ initialFocus: false }}>
-                <motion.div
-                  ref={desktopModalRef}
-                  key="desktop-modal"
-                  className="fixed inset-0 z-40 hidden min-h-screen items-center justify-center md:flex"
-                  initial={{ scale: 0.95 }}
-                  animate={{ scale: 1 }}
-                  exit={{ scale: 0.95 }}
-                  onMouseDown={(e) => {
-                    if (desktopModalRef.current === e.target) {
-                      setShowModal(false);
-                    }
-                  }}
-                >
-                  {children}
-                </motion.div>
-              </FocusTrap>
-              <motion.div
-                key="desktop-backdrop"
-                className="fixed inset-0 z-30 bg-gray-100 bg-opacity-10 backdrop-blur"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                onClick={() => setShowModal(false)}
-              />
-            </>
-          )}
+          {isDesktop && <Leaflet setShow={setShowModal}>{children}</Leaflet> }
         </>
       )}
     </AnimatePresence>
