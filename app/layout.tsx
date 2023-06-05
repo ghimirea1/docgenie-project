63d3760c0a5c37e4c248d41e5a46c0a6a3754706
Ashish Hans Catada Ghimire <ashishg171@gmail.com>
2023-06-04 21:39:51 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,17 +7,9 @@ import Footer from "@/components/layout/footer";
 import { Suspense } from "react";
 
 export const metadata = {
-  title: "Precedent - Building blocks for your Next.js project",
+  title: "DocGenie",
   description:
-    "Precedent is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
-  twitter: {
-    card: "summary_large_image",
-    title: "Precedent - Building blocks for your Next.js project",
-    description:
-      "Precedent is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
-    creator: "@steventey",
-  },
-  metadataBase: new URL("https://precedent.dev"),
+    "",
   themeColor: "#FFF",
 };
 
