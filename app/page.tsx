cba370ff3859202c4267f508b650f432212d9429
Ashish Hans Catada Ghimire <ashishg171@gmail.com>
2023-06-08 16:03:25 -0700

    Update page.tsx
    Co-Authored-By: Wasi M Khan <76685101+wabkhan@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,5 @@
-import { Suspense } from "react";
-import CaseList from "@/components/case/CaseList";
-import CaseListSkeleton from "@/components/case/CaseListSkeleton";
-import EditButton from "@/components/case/EditButton";
-
-const CaseListComponent = CaseList as unknown as () => JSX.Element;
+import Hero from "@/components/layout/Hero";
 
 export default async function Home() {
-    return (
-        <>
-            <div className="containerr">
-                <section className="col sidebar">
-                    <section className="sidebar-header">
-                        <strong>My Cases</strong>
-                    </section>
-                    <section className="sidebar-menu" role="menubar">
-                    <EditButton>Create Case</EditButton>
-                    </section>
-                    <nav>
-                        <Suspense fallback={<CaseListSkeleton />}>
-                            <CaseListComponent />
-                        </Suspense>
-                    </nav>
-                </section>
-                <section className="content"></section>
-            </div>
-        </>
-    );
+    return (<Hero session={null} />);
 }
