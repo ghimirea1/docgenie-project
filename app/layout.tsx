68cc2c7f6ca888e3987839b293b4c46f67ad835e
Ashish Hans Catada Ghimire <ashishg171@gmail.com>
2023-06-07 20:18:26 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import cx from "classnames";
 import { sfPro, inter } from "./fonts";
 import Nav from "@/components/layout/nav";
 import Footer from "@/components/layout/footer";
+import { NextAuthProvider } from "./providers";
 import { Suspense } from "react";
 
 export const metadata = {
@@ -12,15 +13,6 @@ export const metadata = {
     "",
   themeColor: "#FFF",
 };
-
-/* eslint-disable @next/next/no-img-element */
-import CaseList from "@/components/case/CaseList";
-import CaseListSkeleton from "@/components/case/CaseListSkeleton";
-import EditButton from "@/components/case/EditButton";
-import "./globals.css";
-import { NextAuthProvider } from "./providers";
-
-const CaseListComponent = CaseList as unknown as () => JSX.Element;
 
 export default async function RootLayout({
   children }: { 
@@ -42,56 +34,10 @@ export default async function RootLayout({
           <Nav />
         </Suspense>
         </div>
-        <div className="containerr">
-          <section className="col sidebar">
-            <section className="sidebar-header">
-              {/* <img
-                className="logo"
-                src="/icons/svg/logo.svg"
-                width="22px"
-                height="20px"
-                alt=""
-                role="presentation"
-              /> */}
-              <strong>My Cases</strong>
-            </section>
-            <section className="sidebar-menu" role="menubar">
-              <EditButton>Create Case</EditButton>
-            </section>
-            <nav>
-              <Suspense fallback={<CaseListSkeleton />}>
-                <CaseListComponent />
-              </Suspense>
-            </nav>
-          </section>
-          <NextAuthProvider>
-            <section className="content">{children}</section>
-          </NextAuthProvider>
-          </div>
+        <NextAuthProvider>
+          {children}
+        </NextAuthProvider>
       </body>
     </html>
   );
 }
-
-// export default async function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <html lang="en">
-//       <body className={cx(sfPro.variable, inter.variable)}>
-//         <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
-//         <Suspense fallback="...">
-//           {/* @ts-expect-error Server Component */}
-//           <Nav />
-//         </Suspense>
-//         <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
-//           {children}
-//         </main>
-//         <Footer />
-//         <Analytics />
-//       </body>
-//     </html>
-//   );
-// }
