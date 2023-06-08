b866e921929b32c0e9e732cfab4060f6f3691137
Ashish Hans Catada Ghimire <ashishg171@gmail.com>
2023-06-07 20:18:22 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
-import Card from "@/components/home/card";
-import Balancer from "react-wrap-balancer";
-import { DEPLOY_URL } from "@/lib/constants";
-import { Github, Twitter } from "@/components/shared/icons";
-import WebVitals from "@/components/home/web-vitals";
-import ComponentGrid from "@/components/home/component-grid";
-import Image from "next/image";
-import { nFormatter } from "@/lib/utils";
+import { Suspense } from "react";
+import CaseList from "@/components/case/CaseList";
+import CaseListSkeleton from "@/components/case/CaseListSkeleton";
+import EditButton from "@/components/case/EditButton";
+
+const CaseListComponent = CaseList as unknown as () => JSX.Element;
 
 export default async function Home() {
+    return (
+        <>
+            <div className="containerr">
+                <section className="col sidebar">
+                    <section className="sidebar-header">
+                        <strong>My Cases</strong>
+                    </section>
+                    <section className="sidebar-menu" role="menubar">
+                    <EditButton>Create Case</EditButton>
+                    </section>
+                    <nav>
+                        <Suspense fallback={<CaseListSkeleton />}>
+                            <CaseListComponent />
+                        </Suspense>
+                    </nav>
+                </section>
+                <section className="content"></section>
+            </div>
+        </>
+    );
 }
-
